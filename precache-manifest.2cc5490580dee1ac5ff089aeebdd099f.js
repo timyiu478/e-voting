ff8bafa662b07c7f0e56c28fe9a2f1016f4e1f98
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2022-02-28 02:16:07 +0800

    Updates

---
+++ precache-manifest.2cc5490580dee1ac5ff089aeebdd099f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "21bc64cfcb1d94e044d9",
+    "url": "/e-voting/static/css/main.49b25559.chunk.css"
+  },
+  {
+    "revision": "21bc64cfcb1d94e044d9",
+    "url": "/e-voting/static/js/main.21bc64cf.chunk.js"
+  },
+  {
+    "revision": "1d020b448bcbadeb9700",
+    "url": "/e-voting/static/js/1.1d020b44.chunk.js"
+  },
+  {
+    "revision": "cbfec359b6e337999531",
+    "url": "/e-voting/static/css/2.4ebd44ad.chunk.css"
+  },
+  {
+    "revision": "cbfec359b6e337999531",
+    "url": "/e-voting/static/js/2.cbfec359.chunk.js"
+  },
+  {
+    "revision": "8fdb8db9e57399fafad5",
+    "url": "/e-voting/static/js/runtime~main.8fdb8db9.js"
+  },
+  {
+    "revision": "8e186d482ef6c012b48c35e9e3c81d3f",
+    "url": "/e-voting/index.html"
+  }
+];
